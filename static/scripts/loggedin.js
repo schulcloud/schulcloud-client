96d22569024164e54fe25b419579d978c3e1c1f7
schurigh <hannes.schurig@hpi.de>
2018-06-29 17:10:10 +0200

    double eventhandler cleanup with adrian
    off-topic but too lazy to switch/create branches

--- static/scripts/loggedin.js
+++ static/scripts/loggedin.js
@@ -2,6 +2,15 @@ if (window.opener && window.opener !== window) {
     window.isInline = true;
 }
 
+function hidePreloadScreen(e) {
+    const loaderClassList = document.querySelector(".preload-screen").classList;
+    loaderClassList.add("hidden");
+}
+function showPreloadScreen(e) {
+    const loaderClassList = document.querySelector(".preload-screen").classList;
+    loaderClassList.remove("hidden");
+}
+
 const diffDOM = new diffDOM();
 
 function softNavigate(newurl, selector = 'html', listener, callback) {
@@ -73,6 +82,7 @@ $(document).ready(function () {
     // Init mobile nav
     var mobileNavToggle = document.querySelector('.mobile-nav-toggle');
     var mobileSearchToggle = document.querySelector('.mobile-search-toggle');
+    // seperate functions to prevent doubled event handler
     if (mobileNavToggle) {
         mobileNavToggle.addEventListener('click', toggleMobileNav);
     }
@@ -202,12 +212,6 @@ $(document).ready(function () {
         $qrbox.empty();
         $qrbox.append(image);
     });
-  
-    // Init mobile nav
-    if (document.getElementById('searchBar') instanceof Object) {
-        document.querySelector('.mobile-nav-toggle').addEventListener('click', toggleMobileNav);
-        document.querySelector('.mobile-search-toggle').addEventListener('click', toggleMobileSearch);
-    }
 
     if (!fullscreen) {
         fullscreen = JSON.parse(sessionStorage.getItem("fullscreen")) || false;
@@ -215,6 +219,7 @@ $(document).ready(function () {
             togglePresentationMode()
         }
     }
+    // seperate functions to prevent doubled event handler
     if(document.querySelector('.btn-fullscreen')){
         document.querySelector('.btn-fullscreen').addEventListener('click', fullscreenBtnClicked);
     }
@@ -235,15 +240,9 @@ $(document).ready(function () {
         closeLabel: 'Abbrechen'
     });
   
-    // loading animation
-    window.addEventListener("beforeunload", function (e) {
-        const loaderClassList = document.querySelector(".preload-screen").classList;
-        loaderClassList.remove("hidden");
-    });
-    window.addEventListener("pageshow", function (e) {
-        const loaderClassList = document.querySelector(".preload-screen").classList;
-        loaderClassList.add("hidden");
-    });
+    // loading animation, seperate functions to prevent doubled event handler
+    window.addEventListener("beforeunload", showPreloadScreen);
+    window.addEventListener("pageshow", hidePreloadScreen);
 
     // from: https://stackoverflow.com/a/187557
     jQuery.expr[":"].Contains = jQuery.expr.createPseudo(function (arg) {
