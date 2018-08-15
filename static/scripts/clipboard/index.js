6cf6de96b2ce0257a7290803889d9cab540aa546
Michael Janke <micha@jankeweb.de>
2018-08-15 17:53:21 +0200

    websocket namespace for clipboard

--- static/scripts/clipboard/index.js
+++ static/scripts/clipboard/index.js
@@ -6,9 +6,10 @@ const appDiv = $('#clipboard-app')[0];
 
 $(document).ready(function () {
     let url = appDiv.getAttribute('data-backend-url');
+    console.log(url);
     let courseId = appDiv.getAttribute('data-course-id');
     window.websocketUrl = url;
-    window.clipboardSocket = io(url,  {
+    window.clipboardSocket = io(url + '/clipboard',  {
         query: "courseId=" + courseId,
         reconnection: true,
         reconnectionDelay: 1000,
@@ -23,3 +24,4 @@ $(document).ready(function () {
         appDiv
     );
 });
+
