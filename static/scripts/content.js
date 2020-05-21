d7248e29261dad67f83231a1586b02f4e3c5c647
Artur <bialoskorski.artur@gmail.com>
2020-05-21 15:49:41 +0200

    added more styling to modal and changed naming

--- static/scripts/content.js
+++ static/scripts/content.js
@@ -1,39 +1,48 @@
-import { softNavigate } from './helpers/navigation';
-import { updateQueryStringParameter } from './helpers/queryStringParameter';
+import { softNavigate } from "./helpers/navigation";
+import { updateQueryStringParameter } from "./helpers/queryStringParameter";
 
-
-function loadSearchResult(event){
+function loadSearchResult(event) {
 	event.preventDefault();
-	const newUrl = updateQueryStringParameter("q",document.querySelector(".search-field").value, true);
+	const newUrl = updateQueryStringParameter(
+		"q",
+		document.querySelector(".search-field").value,
+		true
+	);
 	softNavigate(newUrl, ".ajaxcontent", ".pagination");
 }
 
-window.addEventListener("DOMContentLoaded", function(){
-    /* FEATHERS FILTER MODULE */
-    const filterModule = document.getElementById("filter");
-    if(filterModule){
-        filterModule.addEventListener('newFilter', (e) => {
-            const filter = e.detail;
-			const newurl = `?q=${document.querySelector('.search-field').value}&filterQuery=${escape(JSON.stringify(filter[0]))}`;
-            softNavigate(newurl, ".ajaxcontent", ".pagination");
-        });
-        document.querySelector(".filter").dispatchEvent(new CustomEvent("getFilter"));
+window.addEventListener("DOMContentLoaded", function () {
+	/* FEATHERS FILTER MODULE */
+	const filterModule = document.getElementById("filter");
+	if (filterModule) {
+		filterModule.addEventListener("newFilter", (e) => {
+			const filter = e.detail;
+			const newurl = `?q=${
+				document.querySelector(".search-field").value
+			}&filterQuery=${escape(JSON.stringify(filter[0]))}`;
+			softNavigate(newurl, ".ajaxcontent", ".pagination");
+		});
+		document
+			.querySelector(".filter")
+			.dispatchEvent(new CustomEvent("getFilter"));
 	}
 
 	/* SOFT NAVIGATE SEARCH */
 	const searchField = document.querySelector(".search-field");
-	if(document.querySelector('#filter') && searchField){
-		searchField.closest("form").addEventListener("submit", loadSearchResult);
+	if (document.querySelector("#filter") && searchField) {
+		searchField
+			.closest("form")
+			.addEventListener("submit", loadSearchResult);
 	}
 });
 
 $(document).ready(function () {
-	var $modals = $('.modal');
-	var $editModal = $('.edit-modal');
-	var $externalLinkModal = $('.external-link-modal');
+	var $modals = $(".modal");
+	var $editModal = $(".edit-modal");
+	var $externalLinkModal = $(".external-link-modal");
 
 	var populateCourseSelection = function (modal, courses) {
-		var $selection = modal.find('.course-selection');
+		var $selection = modal.find(".course-selection");
 		courses.forEach(function (course) {
 			var option = document.createElement("option");
 			option.text = course.name;
@@ -44,11 +53,8 @@ $(document).ready(function () {
 	};
 
 	var populateLessonSelection = function (modal, lessons) {
-		var $selection = modal.find('.lesson-selection');
-		$selection
-			.find('option')
-			.remove()
-			.end();
+		var $selection = modal.find(".lesson-selection");
+		$selection.find("option").remove().end();
 
 		lessons.forEach(function (lesson) {
 			var option = document.createElement("option");
@@ -57,61 +63,64 @@ $(document).ready(function () {
 			$selection.append(option);
 		});
 
-		modal.find('.lessons').css("display", "block");
+		modal.find(".lessons").css("display", "block");
 		$selection.chosen().trigger("chosen:updated");
 	};
 
-	$('.add-to-lesson').on('click', function (e) {
+	$(".add-to-lesson").on("click", function (e) {
 		e.preventDefault();
-		var entry = $(this).attr('href');
-		var query = $('.search-field');
+		var entry = $(this).attr("href");
+		var query = $(".search-field");
 
 		$.getJSON(entry, function (result) {
 			var fields = result.content;
 			fields.query = query.val();
 
-			if(window.isInline) {
-					window.opener.addResource({
-						url: fields.url,
-						title: fields.title,
-						description: fields.description,
-						client: fields.providerName
-					});
-					window.close();
-					return;
+			if (window.isInline) {
+				window.opener.addResource({
+					url: fields.url,
+					title: fields.title,
+					description: fields.description,
+					client: fields.providerName,
+				});
+				window.close();
+				return;
 			}
 
 			populateModalForm($editModal, {
-				title: 'Material zu Thema hinzufügen',
-				closeLabel: 'Abbrechen',
-				submitLabel: 'Senden',
-				fields: fields
+				title: "Material zu Thema hinzufügen",
+				closeLabel: "Abbrechen",
+				submitLabel: "Hinzufügen",
+				fields: fields,
 			});
 			populateCourseSelection($editModal, result.courses);
-			$editModal.appendTo('body').modal('show');
+			$editModal.appendTo("body").modal("show");
 		});
-
 	});
 
-	$('.course-selection').on('change', function () {
+	$(".course-selection").on("change", function () {
 		var selectedCourse = $(this).find("option:selected").val();
-		$.getJSON('/courses/' + selectedCourse + '/json', function (res) {
+		$.getJSON("/courses/" + selectedCourse + "/json", function (res) {
 			populateLessonSelection($editModal, res.lessons.data);
 		});
 	});
 
-	$('.external-link').on('click', function () {
+	$(".external-link").on("click", function () {
 		populateModalForm($externalLinkModal, {
-				title: 'Sie verlassen jetzt die Schul-Cloud',
-				closeLabel: 'Abbrechen',
+			title: "Sie verlassen jetzt die Schul-Cloud",
+			closeLabel: "Abbrechen",
 		});
-		$externalLinkModal.find('.external-link-btn').attr('href', $(this).data('external-link'));
-		var provider = $externalLinkModal.find('.provider');
-		provider.html($(this).data('provider') || provider.html());
-		$externalLinkModal.appendTo('body').modal('show');
+		$externalLinkModal
+			.find(".external-link-btn")
+			.attr("href", $(this).data("external-link"));
+		var provider = $externalLinkModal.find(".provider");
+		provider.html($(this).data("provider") || provider.html());
+		$externalLinkModal.appendTo("body").modal("show");
 	});
 
-	$modals.find('.close, .btn-close, .external-link-btn').on('click', function () {
-		$modals.modal('hide');
-	});
+	$modals
+		.find(".close, .btn-close, .external-link-btn")
+		.on("click", function () {
+			$modals.modal("hide");
+		});
 });
